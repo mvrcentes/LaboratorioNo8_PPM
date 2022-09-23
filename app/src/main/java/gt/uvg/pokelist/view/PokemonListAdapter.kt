4554315495a4e05072144a191aec2c02bdbb6368
Marco Ramirez <mvrcentes@hotmail.com>
2022-09-23 07:45:23 -0600

    Código base

---
+++ app/src/main/java/gt/uvg/pokelist/view/PokemonListAdapter.kt
@@ -0,0 +1,36 @@
+
+package gt.uvg.pokelist.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import gt.uvg.pokelist.databinding.ItemPokemonViewBinding
+import gt.uvg.pokelist.model.Pokemon
+
+class PokemonListAdapter(private val pokemonList: List<Pokemon>) : RecyclerView.Adapter<PokemonListAdapter.PokemonListHolder>() {
+
+    inner class PokemonListHolder(val binding: ItemPokemonViewBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonListHolder {
+        val binding = ItemPokemonViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PokemonListHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: PokemonListHolder, position: Int) {
+        val pokemon = pokemonList.get(position)
+        holder.binding.pokemonName.text = pokemon.name
+        Picasso.get().load(pokemon.imageUrlFront).into(holder.binding.pokemonPhoto)
+
+        holder.binding.root.setOnClickListener{
+            val action = MainFragmentDirections.actionMainFragmentToDetailFragment(pokemon.id)
+            holder.itemView.findNavController().navigate(action)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return pokemonList.size
+    }
+}
